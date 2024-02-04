7a164c158a90478dda5003e472889371cf8f958f
Nazım Çimen <cimennazim.27@gmail.com>
2024-02-04 14:40:21 +0300

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Bulut Bilişim ve DevOps Teknolojileri Dersi Ödevi'
+    return 'Bulut Bilişim ve DevOps Teknolojileri Dersi Ödevi-merhaba'
 
 if __name__ == '__main__':
     app.run()
